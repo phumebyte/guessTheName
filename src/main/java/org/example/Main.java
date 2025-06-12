3f8fce06622d050fa86494b2cb92ea0373ef6ea7
Phumelele Dlamini <phumelele.dlamini1000@gmail.com>
2025-06-12 09:19:09 +0200

    We did it GUYS!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,29 +2,33 @@ package org.example;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        int attemptCount = 0;
+        int attemptCount = 3;
 
         Guess guessName = new Guess("Guess my name!!!!", "Hlulani", "Phume", "DeeCoder", "Tshimo", "Anonymous", "Anonymous");
         guessName.display();
 
-        System.out.println("Enter a name!!!");
-
         Scanner scanInput = new Scanner(System.in);
-        String answer = scanInput.nextLine();
 
-        while (!guessName.compareAnswer(answer)){
-            attemptCount++;
-            System.out.println("WRONG! Try again, LOSER");
-            guessName.display();
-            answer = scanInput.nextLine();
-            guessName.compareAnswer(answer);
-            System.out.println(attemptCount);
+        while (attemptCount > 0) {
+            System.out.println("Enter a name:");
+            String answer = scanInput.nextLine();
+
+            if (guessName.compareAnswer(answer)) {
+                System.out.println("Correct! Well done WINNER!");
+                break;
+            } else {
+                attemptCount--;
+                if (attemptCount > 0) {
+                    System.out.println("Wrong LOSER! Try again. You have " + attemptCount + " attempt/s left.");
+                    guessName.display();
+                } else {
+                    System.out.println("You've run out of attempts. LOSER");
+                }
+            }
         }
+
         scanInput.close();
     }
 }
